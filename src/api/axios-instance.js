87d00ef78f154a41aa336cda04c3a63bc6361b0b
Илья Долженко <ilay00@mail.ru>
2020-08-02 22:39:37 +0300

    removed env parameter

--- src/api/axios-instance.js
+++ src/api/axios-instance.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const _axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_BASE_URL
-});
+const _axiosInstance = axios.create();
 
 _axiosInstance.interceptors.request.use(config => {
     const accessToken = localStorage.getItem("accessToken");
