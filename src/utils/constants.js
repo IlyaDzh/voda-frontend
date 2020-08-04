e196535a0692c2110d42a62b18009e9f8cb818f3
Илья Долженко <ilay00@mail.ru>
2020-08-04 15:44:18 +0300

    ssl

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
-export const API_BASE_VALIDATOR = "http://167.71.76.249:3004";
+export const API_BASE_VALIDATOR = "https://167.71.76.249:3004";
 
-export const API_BASE_MART = "http://134.209.94.143:3005";
+export const API_BASE_MART = "https://134.209.94.143:3005";
 
 export const UNREG_MENU_LIST = ["explore-files"];
 
