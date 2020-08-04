00c73c9c1e441d05918ed36e05d0ae1612a9d8a1
Илья Долженко <ilay00@mail.ru>
2020-08-04 11:59:14 +0300

    fixed captcha position

--- src/components/ReCaptcha.js
+++ src/components/ReCaptcha.js
@@ -1,25 +1,27 @@
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
     captcha: {
-        [theme.breakpoints.down("xs")]: {
-            transform: 'scale(0.7)',
-            marginLeft: -40,
+        [theme.breakpoints.down(420)]: {
+            transform: "scale(0.87)",
+            marginLeft: "-13px"
         }
     }
 }));
 
 const ReCaptcha = ({ onChange }) => {
     const classes = useStyles();
+
     return (
-    <ReCAPTCHA
-        sitekey="6Lfd2rIZAAAAABxdf5bcSUAU24FNjVpcv1u3oBs2"
-        onChange={onChange}
-        hl="en"
-        className={classes.captcha}
-    />
-)};
+        <ReCAPTCHA
+            className={classes.captcha}
+            sitekey="6Lfd2rIZAAAAABxdf5bcSUAU24FNjVpcv1u3oBs2"
+            onChange={onChange}
+            hl="en"
+        />
+    );
+};
 
 export default ReCaptcha;
