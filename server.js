43b9ee7a161c90314314d144c372da48e543d169
Илья Долженко <ilay00@mail.ru>
2020-08-04 18:01:32 +0300

    added server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const { config } = require("dotenv");
+config();
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (request, response) => {
+    response.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+console.log("Starting express app");
+
+const port = 3001;
+
+app.listen(port);
+
+console.log(`Express app is listening on port ${port}`);
